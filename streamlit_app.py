a28a223bed63ae2c8c1fcc23c74a412ac3b60780
codertarun123cd <145796313+codertarun123cd@users.noreply.github.com>
2023-09-22 22:53:21 +0530

    Update streamlit_app.py
    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title("Hello my first streamlit app")
+streamlit.title("My Mom's New Healthy Dinner")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
