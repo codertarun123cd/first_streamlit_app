e184936adca72244f5cf9d8dcfadf63731885e75
codertarun123cd <145796313+codertarun123cd@users.noreply.github.com>
2023-09-25 15:28:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import sonwflake.connector
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
